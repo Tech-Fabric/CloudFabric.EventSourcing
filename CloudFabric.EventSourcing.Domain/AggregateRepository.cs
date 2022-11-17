ec33a572fa1a76e32aa124d3a3a3649a69198867
Sergey Ustimenko <sergey@ustimenko.email>
2022-11-17 18:01:16 +0200

    Aggregate repository fir

--- CloudFabric.EventSourcing.Domain/AggregateRepository.cs
+++ CloudFabric.EventSourcing.Domain/AggregateRepository.cs
@@ -14,7 +14,7 @@ public class AggregateRepository<T> : IAggregateRepository<T> where T : Aggregat
 
     public async Task<T?> LoadAsync(Guid id, string partitionKey, CancellationToken cancellationToken = default)
     {
-        if(id == Guid.Empty)
+        if (id == Guid.Empty)
         {
             throw new ArgumentNullException(nameof(id));
         }
@@ -22,9 +22,26 @@ public class AggregateRepository<T> : IAggregateRepository<T> where T : Aggregat
         var eventStream = await _eventStore.LoadStreamAsync(id, partitionKey);
 
         if (!eventStream.Events.Any()) return null;
-        
+
+        return ConstructAggregateInstanceFromEventStream(eventStream);
+    }
+
+    public async Task<T> LoadAsyncOrThrowNotFound(Guid id, string partitionKey, CancellationToken cancellationToken = default)
+    {
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentNullException(nameof(id));
+        }
+
+        var eventStream = await _eventStore.LoadStreamAsyncOrThrowNotFound(id, partitionKey);
+
+        return ConstructAggregateInstanceFromEventStream(eventStream);
+    }
+
+    private T ConstructAggregateInstanceFromEventStream(EventStream eventStream)
+    {
         var firstEvent = eventStream.Events.First();
-            
+
         // Support for derived types. The construction of generic T here will not work if 
         // our aggregate is one of many derived types of T. 
         // Hence we are storing exact aggregate type in each event to be able to construct 
@@ -35,24 +52,19 @@ public class AggregateRepository<T> : IAggregateRepository<T> where T : Aggregat
 
             if (type != null)
             {
-                return (T?)Activator.CreateInstance(type, new object[] { eventStream.Events });
+                return (T?)Activator.CreateInstance(type, new object[] { eventStream.Events }) ??
+                       throw new InvalidOperationException(
+                           "Unable to construct Aggregate instance of type " +
+                           $"{firstEvent.AggregateType}"
+                       );
             }
         }
-            
-        return (T?)Activator.CreateInstance(typeof(T), new object[] { eventStream.Events });
 
-    }
-
-    public async Task<T> LoadAsyncOrThrowNotFound(Guid id, string partitionKey, CancellationToken cancellationToken = default)
-    {
-        if(id == Guid.Empty)
-        {
-            throw new ArgumentNullException(nameof(id));
-        }
-
-        var eventStream = await _eventStore.LoadStreamAsyncOrThrowNotFound(id, partitionKey);
-
-        return (T)Activator.CreateInstance(typeof(T), new object[] { eventStream.Events });
+        return (T?)Activator.CreateInstance(typeof(T), new object[] { eventStream.Events }) ??
+               throw new InvalidOperationException(
+                   "Unable to construct aggregate instance of type " +
+                   $"{typeof(T).AssemblyQualifiedName}"
+               );
     }
 
     public async Task<bool> SaveAsync(EventUserInfo eventUserInfo, T aggregate, CancellationToken cancellationToken = default)
