e287edc36e9ae070eb3f420b9f1430c877ef889d
Sergey Ustimenko <sergey@ustimenko.email>
2022-12-27 17:57:31 +0200

    ProjectionQueryResult: copy method that allows changing result documents

--- CloudFabric.Projections/Queries/ProjectionQueryResult.cs
+++ CloudFabric.Projections/Queries/ProjectionQueryResult.cs
@@ -16,6 +16,16 @@ public class QueryResultDocument<T>
     public Dictionary<string, List<string>> Highlights { get; set; } = new Dictionary<string, List<string>>();
     public T? Document { get; set; }
 
+    public QueryResultDocument<TNew> TransformResultDocument<TNew>(Func<T, TNew?> transformFunction)
+    {
+        return new QueryResultDocument<TNew>()
+        {
+            Score = Score,
+            Highlights = Highlights,
+            Document = Document != null ? transformFunction(Document) : default(TNew)
+        };
+    }
+
     public string? GetHighlightedTextForField(string fieldName)
     {
         if (string.IsNullOrEmpty(fieldName) || !Highlights.ContainsKey(fieldName) || Highlights[fieldName].Count < 1)
@@ -34,4 +44,15 @@ public class ProjectionQueryResult<T>
     public string? QueryId;
     public List<QueryResultDocument<T>> Records = new List<QueryResultDocument<T>>();
     public long? TotalRecordsFound = 0;
+
+    public ProjectionQueryResult<TNew> TransformResultDocuments<TNew>(Func<T, TNew> transformFunction)
+    {
+        var result = new ProjectionQueryResult<TNew>();
+        result.FacetsStats = FacetsStats;
+        result.IndexName = IndexName;
+        result.QueryId = QueryId;
+        result.TotalRecordsFound = TotalRecordsFound;
+        result.Records = Records.Select(r => r.TransformResultDocument<TNew>(transformFunction)).ToList();
+        return result;
+    }
 }
