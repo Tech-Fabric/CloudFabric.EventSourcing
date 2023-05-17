bec5a223a13ad319086f73e1659abe331d9c2777
Sergey Ustimenko <sergey@ustimenko.email>
2023-05-17 18:43:18 +0300

    Chores

--- CloudFabric.EventSourcing.AspNet/CloudFabric.EventSourcing.AspNet/EventSourcingBuilder.cs
+++ CloudFabric.EventSourcing.AspNet/CloudFabric.EventSourcing.AspNet/EventSourcingBuilder.cs
@@ -16,4 +16,38 @@ public class EventSourcingBuilder : IEventSourcingBuilder
     public ProjectionsEngine ProjectionsEngine { get; set; }
 
     public IEventsObserver ProjectionEventsObserver { get; set; }
+
+    public dynamic ConstructProjectionBuilder(Type projectionBuilderType, ProjectionRepositoryFactory projectionsRepositoryFactory)
+    {
+        dynamic? projectionBuilder = null;
+        // There are two types of projection builders: 
+        // First one is ProjectionBuilder<ProjectionDocument> and works with strict projection documents represented by class
+        // Second one is just ProjectionBuilder - those projections do not have strict schema and work with raw dictionary {key: value} type of documents.
+        if (projectionBuilderType?.BaseType?.GenericTypeArguments[0]?.BaseType == typeof(ProjectionDocument))
+        {
+            projectionBuilder = (IProjectionBuilder<ProjectionDocument>?)Activator.CreateInstance(
+                projectionBuilderType, new object[]
+                {
+                    projectionsRepositoryFactory, this.AggregateRepositoryFactory
+                }
+            );
+        }
+        else
+        {
+            projectionBuilder = (IProjectionBuilder)Activator.CreateInstance(
+                projectionBuilderType, new object[]
+                {
+                    projectionsRepositoryFactory, this.AggregateRepositoryFactory
+                }
+            );
+        }
+
+        if (projectionBuilder == null)
+        {
+            throw new Exception("Failed to create projection builder instance: Activator.CreateInstance returned null");
+        }
+
+        return projectionBuilder;
+    }
+
 }
