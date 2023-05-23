196f5b01b01175b9aacf9c9cebd87ad0f9f4d869
Sergey Ustimenko <sergey@ustimenko.email>
2023-05-23 15:50:12 +0300

    Additional support for guids

--- CloudFabric.Projections/ProjectionDocuments/ProjectionDocumentPropertySchema.cs
+++ CloudFabric.Projections/ProjectionDocuments/ProjectionDocumentPropertySchema.cs
@@ -1,9 +1,22 @@
 namespace CloudFabric.Projections;
 
+public enum ObjectTypeHintEnum
+{
+    Guid = 0
+}
+
 public class ProjectionDocumentPropertySchema
 {
     public string PropertyName { get; set; }
     public TypeCode PropertyType { get; set; }
+    
+    /// <summary>
+    /// There are types basic to dotnet that don't have their own TypeCode.
+    /// Good example is Guid - GetTypeCode returns `Object` for guids, but we can't store and deserialize it using simple object rules with properties.
+    /// Hence, for such cases there is an additional ObjectTypeHint value - this way we can control edge cases for serializing/deserializing values;
+    /// </summary>
+    public ObjectTypeHintEnum? ObjectTypeHint { get; set; } 
+    
     public bool IsKey { get; set; } = false;
     public bool IsSearchable { get; set; } = false;
     public bool IsRetrievable { get; set; } = true;
@@ -21,6 +34,12 @@ public class ProjectionDocumentPropertySchema
     public bool IsNestedObject { get; set; } = false;
     public bool IsNestedArray { get; set; } = false;
     public TypeCode? ArrayElementType { get; set; }
+    
+    /// <summary>
+    /// See ObjectTypeHint property.
+    /// </summary>
+    public ObjectTypeHintEnum? ArrayElementTypeObjectTypeHint { get; set; }
+    
     // nested object/array properties schema
     public List<ProjectionDocumentPropertySchema> NestedObjectProperties { get; set; }
 
