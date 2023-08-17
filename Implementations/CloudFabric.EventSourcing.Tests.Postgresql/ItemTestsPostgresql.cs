8ab4d8090f6a4b430fa6969866688884e4ef7ccc
Dmitriy Melnychenko <dmitriy.melnichenko@techfabric.com>
2023-08-17 15:07:10 +0300

    Push unsaved changes

--- Implementations/CloudFabric.EventSourcing.Tests.Postgresql/ItemTestsPostgresql.cs
+++ Implementations/CloudFabric.EventSourcing.Tests.Postgresql/ItemTestsPostgresql.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CloudFabric.EventSourcing.Tests.Postgresql;
 
 [TestClass]
-public class ItemTestPostgresql : ItemTests
+public class ItemTestsPostgresql : ItemTests
 {
     private PostgresqlStore? _store;
 
